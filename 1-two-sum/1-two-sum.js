b5da132f06df1fc43d2dd89d0f3d0dc19eb9b0d4
Will Kee <93359204+willkee@users.noreply.github.com>
2022-05-04 11:39:47 -0600

    Time: 95 ms (65.15%), Space: 45.1 MB (8.28%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -5,13 +5,21 @@
  */
 const twoSum = (nums, target) => {
     const res = [];
+    // for (let i = 0; i < nums.length; i++) {
+    //     for (let j = i + 1; j < nums.length; j++) {
+    //         if (nums[i] + nums[j] === target) {
+    //             res.push(i)
+    //             res.push(j)
+    //         }
+    //     }
+    // }
+    
+    const numsSet = new Set(nums);
+    
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                res.push(i)
-                res.push(j)
-            }
+        let remainder = target - nums[i];
+        if (numsSet.has(remainder) && nums.indexOf(remainder) !== i) {
+            return [i, nums.indexOf(remainder)]
         }
     }
-    return res;
 };
