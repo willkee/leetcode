b8c47e53196e9f8fe53d11dbee496facb1aafe59
Will Kee <93359204+willkee@users.noreply.github.com>
2022-05-08 16:58:37 -0600

    Time: 75 ms (98.47%), Space: 47.7 MB (61.91%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,27 @@
+const lengthOfLongestSubstring = s => {
+    if (s.length === 1) return 1;
+    
+    let curr = "";
+    let longest = "";
+    
+    for (let i = 0; i < s.length; i++) {
+        let j = curr.indexOf(s[i]);
+        
+        if (j === -1) {
+            // Letter does not exist in the current string--add to string.
+            curr += s[i];
+        } else {
+            // Letter exists:
+            if (curr.length > longest.length) {
+                // If the length of current string is longer than length of "longest" string,
+                // replace longest string with current string.
+                longest = curr;
+            }
+            curr = curr.slice(j + 1) + s[i];
+        }
+
+    }
+
+    if (curr.length > longest.length) longest = curr;
+    return longest.length;
+};
