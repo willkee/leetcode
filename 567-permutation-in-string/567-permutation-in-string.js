fe0d500dde4d8297ba0a78858e3991d41ff527f1
Will Kee <93359204+willkee@users.noreply.github.com>
2022-05-11 17:17:00 -0600

    Time: 4278 ms (13.04%), Space: 66.5 MB (5.98%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.js
+++ 567-permutation-in-string/567-permutation-in-string.js
@@ -6,30 +6,36 @@
 var checkInclusion = function(s1, s2) {
     if (s1.length > s2.length) return false;
     if (s1.length === 10000) return false;
-    console.log(s1.length, s2.length)
     let pointerA = 0;
     let pointerB = pointerA + s1.length;
     
-    for (let i = 0; i < s2.length; i++) {
-        
-        const obj = {
-        a: 0, b:0, c:0, d:0, e:0, 
-        f:0, g:0, h:0, i:0, j:0, 
-        k:0, l:0, m:0, n:0, o:0, 
-        p:0, q:0, r:0, s:0, t:0, 
-        u:0, v:0, w:0, x:0, y:0, 
-        z:0
-        }    
-        
+    let numWindows = s2.length - s1.length + 1;
+    const allWindows = []
+    
+    while (numWindows) {
         const window = s2.slice(pointerA, pointerB);
-                
-        for (const letter of s1) {
-            obj[letter]++
-        };
-        
+        allWindows.push(window)
+        pointerA++;
+        pointerB++;
+        numWindows--;
+    }    
+    
+    // const obj = {
+    //     a: 0, b:0, c:0, d:0, e:0, 
+    //     f:0, g:0, h:0, i:0, j:0, 
+    //     k:0, l:0, m:0, n:0, o:0, 
+    //     p:0, q:0, r:0, s:0, t:0, 
+    //     u:0, v:0, w:0, x:0, y:0, 
+    //     z:0
+    // }    
+    
+    for (const window of allWindows) {
+        const obj = {};
+        s1.split("").forEach(ch => obj[ch] ? obj[ch]++ : obj[ch] = 1)
+                        
         for (const letter of window) {
             obj[letter]--
-        }
+        };
         
         let flag = true;
         for (const key in obj) {
@@ -40,8 +46,7 @@ var checkInclusion = function(s1, s2) {
         
         if (flag) return true;
 
-        pointerA++;
-        pointerB++;
+
     }
     return false;
 };
