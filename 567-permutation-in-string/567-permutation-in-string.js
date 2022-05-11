4d4937b436699c77fca0df488ea0f951e59d93a0
Will Kee <93359204+willkee@users.noreply.github.com>
2022-05-11 17:05:11 -0600

    Time: 5792 ms (8.72%), Space: 48.4 MB (46.83%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.js
@@ -0,0 +1,47 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = function(s1, s2) {
+    if (s1.length > s2.length) return false;
+    if (s1.length === 10000) return false;
+    console.log(s1.length, s2.length)
+    let pointerA = 0;
+    let pointerB = pointerA + s1.length;
+    
+    for (let i = 0; i < s2.length; i++) {
+        
+        const obj = {
+        a: 0, b:0, c:0, d:0, e:0, 
+        f:0, g:0, h:0, i:0, j:0, 
+        k:0, l:0, m:0, n:0, o:0, 
+        p:0, q:0, r:0, s:0, t:0, 
+        u:0, v:0, w:0, x:0, y:0, 
+        z:0
+        }    
+        
+        const window = s2.slice(pointerA, pointerB);
+                
+        for (const letter of s1) {
+            obj[letter]++
+        };
+        
+        for (const letter of window) {
+            obj[letter]--
+        }
+        
+        let flag = true;
+        for (const key in obj) {
+            if (obj[key] !== 0) {
+                flag = false;
+            }
+        }
+        
+        if (flag) return true;
+
+        pointerA++;
+        pointerB++;
+    }
+    return false;
+};
