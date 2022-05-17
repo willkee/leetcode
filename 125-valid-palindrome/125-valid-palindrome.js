f9eb0f7e20bc98f4b26fc6f973d186777a34e56d
Will Kee <93359204+willkee@users.noreply.github.com>
2022-05-17 14:49:24 -0600

    Time: 91 ms (61.01%), Space: 48.5 MB (24.34%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -5,16 +5,38 @@
 var isPalindrome = function(s) {
     const alphaNum = new Set(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
     
-    let copy = s.slice().toLowerCase().split("");
+//     let copy = s.slice().toLowerCase().split("");
     
-    const arr = [];
+//     const arr = [];
     
-    while (copy.length) {
-        let current = copy.pop();
-        if (alphaNum.has(current)) {
-            arr.push(current);
+//     while (copy.length) {
+//         let current = copy.pop();
+//         if (alphaNum.has(current)) {
+//             arr.push(current);
+//         }
+//     }
+    
+//     return arr.join("") === arr.reverse().join("")
+    
+    let left = 0;
+    let right = s.length - 1;
+    
+    while (left < right) {
+        if (!alphaNum.has(s[left].toLowerCase())) {
+            left++;
+            continue
         }
+        if (!alphaNum.has(s[right].toLowerCase())) {
+            right--;
+            continue
+        }
+        if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+            console.log(s[left], s[right])
+            return false;
+        }
+        left++;
+        right--;
     }
     
-    return arr.join("") === arr.reverse().join("")
+    return true;
 };
