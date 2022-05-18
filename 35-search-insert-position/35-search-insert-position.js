5d2c6a87b390451d5faf61022e1b63f4a1962321
Will Kee <93359204+willkee@users.noreply.github.com>
2022-05-17 20:41:10 -0600

    Time: 86 ms (35.84%), Space: 44 MB (5.44%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -4,24 +4,12 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    if (!nums.includes(target)) {
+    if (nums.indexOf(target) !== -1) {
+        return nums.indexOf(target);
+    } else {
         nums.push(target);
-        nums.sort((a, b) => a - b)
+        return nums.sort((a, b) => a - b).indexOf(target)
     }
     
-        let low = 0;
-        let high = nums.length - 1;
-        
-        while (low <= high) {
-            let mid = Math.floor((low + high)/2);
-            
-            if (nums[mid] === target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-
+    
 };
