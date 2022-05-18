bd22ab256ddac84921da8517c955de0e63dfbedc
Will Kee <93359204+willkee@users.noreply.github.com>
2022-05-17 20:37:49 -0600

    Time: 83 ms (42.36%), Space: 41.9 MB (61.18%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.js
+++ 278-first-bad-version/278-first-bad-version.js
@@ -17,20 +17,18 @@ var solution = function(isBadVersion) {
      * @param {integer} n Total versions
      * @return {integer} The first bad version
      */
-    return function(n) {
-        
+    return function(n) {   
         let l = 0;
-        let r = n;
         let first;
         
-        while (l <= r) {
-            let mid = Math.floor((l + r)/2);
+        while (l <= n) {
+            let mid = Math.floor((l + n)/2);
             
             if (!isBadVersion(mid)) {
                 l = mid + 1;
             } else {
                 first = mid;
-                r = mid - 1;
+                n = mid - 1;
             }
         }
         return first;
