c5260a10c0e19f1fa59771cc48a5b387da8d82b1
Will Kee <93359204+willkee@users.noreply.github.com>
2022-05-31 16:32:10 -0600

    Time: 71 ms (82.36%), Space: 42.9 MB (40.70%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.js
+++ 152-maximum-product-subarray/152-maximum-product-subarray.js
@@ -11,15 +11,20 @@ var maxProduct = function(nums) {
     
     let min = 1;
     let max = 1;
-    let global = -Infinity;
+    let global = Math.max(...nums);
     
     for (let i = 0; i < nums.length; i++) {
         let n = nums[i];
         let temp = max * n;
         
-        max = Math.max(temp, min * n, n)
-        min = Math.min(temp, min * n, n)
-        global = Math.max(global, max)
+        if (n === 0) {
+            min = 1;
+            max = 1;
+        } else {
+            max = Math.max(temp, min * n, n)
+            min = Math.min(temp, min * n, n)
+            global = Math.max(global, max)
+        }
     }
     
     return global;
