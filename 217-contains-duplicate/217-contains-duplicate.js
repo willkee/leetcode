f30a37608cbb9bd1d39b5f049370fba60c8cee82
Will Kee <93359204+willkee@users.noreply.github.com>
2022-06-01 14:21:00 -0600

    Time: 96 ms (80.22%), Space: 50.9 MB (59.30%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -2,29 +2,29 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function(nums) {
-    /*
-    Prediction:
-    Time: O(n)
-    Space: O(n)
+// var containsDuplicate = function(nums) {
+//     /*
+//     Prediction:
+//     Time: O(n)
+//     Space: O(n)
     
-    Initialize an empty object
-    Iterate through the array
-    If the key does not exist in the obj, add to object
-    If key exists, return true
-    return false outside of the for loop
+//     Initialize an empty object
+//     Iterate through the array
+//     If the key does not exist in the obj, add to object
+//     If key exists, return true
+//     return false outside of the for loop
     
 
-    */
-    const unique = new Set(nums);
-    return Array.from(unique).length !== nums.length
+//     */
+//     return Array.from(new Set(nums)).length !== nums.length
 
-    // for (const num of nums) {
-    //     if (!unique.has(num)) {
-    //         unique.add(num)
-    //     } else {
-    //         return true
-    //     }
-    // }
-    // return false
-};
+//     // for (const num of nums) {
+//     //     if (!unique.has(num)) {
+//     //         unique.add(num)
+//     //     } else {
+//     //         return true
+//     //     }
+//     // }
+//     // return false
+// };
+ const containsDuplicate = nums => Array.from(new Set(nums)).length !== nums.length
