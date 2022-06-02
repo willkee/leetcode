0af239530427b2a08bd8c75371b5ebbada7fce34
Will Kee <93359204+willkee@users.noreply.github.com>
2022-06-02 16:47:05 -0600

    Time: 87 ms (47.65%), Space: 45.4 MB (21.01%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -4,5 +4,19 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    return nums.indexOf(target)
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while (left <= right) {
+        let m = Math.floor((left + right)/2);
+        
+        if (nums[m] === target) return m;
+        
+        if (nums[m] > target) {
+            right = m - 1
+        } else {
+            left = m + 1
+        }
+    }
+    return -1;
 };
