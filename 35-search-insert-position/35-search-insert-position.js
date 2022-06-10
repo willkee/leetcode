25e8f1c1d2186b482c81186531e9186c16be709e
Will Kee <93359204+willkee@users.noreply.github.com>
2022-06-09 20:12:00 -0600

    Time: 75 ms (58.61%), Space: 42.1 MB (73.41%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -4,14 +4,6 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-//     if (!nums.includes(target)) {
-//         nums.push(target)
-//     }
-//     nums.sort((a, b) => a - b)
-    
-//     for (let i = 0; i < nums.length; i++) {
-//         if (nums[i] === target) return i
-//     } 
     if (nums[nums.length - 1] < target) return nums.length
     if (nums[0] > target) return 0;
     
