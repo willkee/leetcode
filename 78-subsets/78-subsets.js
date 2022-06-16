b9fc8e6ae03432371207edad70093220db58acc9
Will Kee <93359204+willkee@users.noreply.github.com>
2022-06-15 20:38:11 -0600

    Time: 75 ms (76.69%), Space: 42.9 MB (87.75%) - LeetHub

---
+++ 78-subsets/78-subsets.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+    const res = [];
+    const subset = [];
+    
+    const dfs = i => {
+        if (i >= nums.length) {
+            res.push(subset.slice())
+            return
+        }
+        
+        subset.push(nums[i])
+        dfs(i + 1)
+        
+        subset.pop()
+        dfs(i + 1)
+    }
+    dfs(0)
+    return res
+};
