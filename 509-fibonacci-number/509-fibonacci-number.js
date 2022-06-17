6542262467fda033337b0a48073f74784e664e54
Will Kee <93359204+willkee@users.noreply.github.com>
2022-06-17 15:05:16 -0600

    Time: 122 ms (23.93%), Space: 43.9 MB (7.17%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -2,39 +2,46 @@
  * @param {number} n
  * @return {number}
  */
-// var fib = function(n) {
-//     /*
+var fib = function(n) {
+    /*
     
-//     Edge cases: 
+    Edge cases: 
     
-//     n === 0; return 0;
-//     n === 1; return 1;
-//     No negative numbers.
+    n === 0; return 0;
+    n === 1; return 1;
+    No negative numbers.
     
-//     I want to solve this with a recursive solution.
+    I want to solve this with a recursive solution.
     
-//     I need a base case which will stop the recursive loop.
+    I need a base case which will stop the recursive loop.
     
-//     Base Case:
+    Base Case:
     
-//     if n is equal to 0, return 0
-//     if n is equal to 1, return 1
-//     if n is equal to 2, return 1
+    if n is equal to 0, return 0
+    if n is equal to 1, return 1
+    if n is equal to 2, return 1
     
-//     The recursive call:
+    The recursive call:
     
-//     Calling fib() with n - 1 
+    Calling fib() with n - 1 
     
-//     fib(n - 1) + fib(n - 2)
+    fib(n - 1) + fib(n - 2)
     
-//     Time complexity: O(2^n)
-//     Space complexity: O(n)
+    Time complexity: O(2^n)
+    Space complexity: O(n)
     
-//     */
+    */
     
-//     if (n <= 1) return n;
+    const memo = {};
     
-//     return fib(n - 1) + fib(n - 2)
-// };
+    const _fib = n => {
+        if (n <= 1) return n;
+        return fib(n - 1) + fib(n - 2)
 
-const fib = n => n <= 1 ? n : fib(n - 1) + fib(n - 2)
+    }
+    
+    return _fib(n)
+};
+
+// One-liner Solution:
+// const fib = n => n <= 1 ? n : fib(n - 1) + fib(n - 2)
