d865a33f4af87bbbc8d7046e0ba8249a98914fcd
Will Kee <93359204+willkee@users.noreply.github.com>
2022-07-07 13:28:39 -0600

    Time: 129 ms (47.22%), Space: 45.1 MB (28.45%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.js
+++ 724-find-pivot-index/724-find-pivot-index.js
@@ -3,12 +3,11 @@
  * @return {number}
  */
 const pivotIndex = (nums) => {
-    const totalSum = nums.reduce((acc, num) => acc += num, 0);
+    const totalSum = nums.reduce((acc, num) => acc + num, 0);
     let leftSum = 0;
     for (let i = 0; i < nums.length; i++) {
         if (leftSum === totalSum - leftSum - nums[i]) return i;
         leftSum += nums[i]
     }
     return -1;
-    
 };
