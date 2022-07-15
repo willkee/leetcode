98723b40a3a7d7705d37f2393a7668cbff94f627
Will Kee <93359204+willkee@users.noreply.github.com>
2022-07-15 15:37:06 -0600

    Time: 219 ms (39.44%), Space: 53.8 MB (43.33%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -2,21 +2,28 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-var groupAnagrams = function(strs) {
+var groupAnagrams = function(words) {
     const pairs = [];
     
-    for (const word of strs) {
-        const sortedWord = word.split("").sort().join("");
-        pairs.push([word, sortedWord])
-    }
+    // for (const word of strs) {
+    //     const sortedWord = word.split("").sort().join("");
+    //     pairs.push([word, sortedWord])
+    // }
     const groups = {};
     
-    for (const pair of pairs) {
-        const originalWord = pair[0];
-        const sortedWord = pair[1];
-              
+    for (const originalWord of words) {
+        const sortedWord = originalWord.split("").sort().join("");
+        
         if (groups[sortedWord]) groups[sortedWord].push(originalWord);
         else groups[sortedWord] = [originalWord]
     }
+    
+//     for (const pair of pairs) {
+//         const originalWord = pair[0];
+//         const sortedWord = pair[1];
+              
+//         if (groups[sortedWord]) groups[sortedWord].push(originalWord);
+//         else groups[sortedWord] = [originalWord]
+//     }
     return Object.values(groups)
 };
