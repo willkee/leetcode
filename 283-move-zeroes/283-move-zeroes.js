c3f9a33b6f9b9892978c873d5bfd669e117a5c7c
Will Kee <93359204+willkee@users.noreply.github.com>
2022-07-15 13:10:43 -0600

    Time: 125 ms (64.58%), Space: 49 MB (5.10%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.js
+++ 283-move-zeroes/283-move-zeroes.js
@@ -1,11 +1,46 @@
-const moveZeroes = nums => {
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    /*
+    
+    Edge cases:
+
+    If the length of nums === 1, then return nums
+    
+    Create a variable, i, to track the index as we iterate through nums.
+    Create a while loop;
+    
+    If the value at nums[i] is 0, we splice the 0 out of the array.
+    Push a 0 to the end.
+    Continue, do not increment "i".
+    
+    Else, increment i.
+    
+    Time complexity: O(n) where n is the number of elements in nums.
+    Space complexity: O(1).
+    
+    */
+    
+    if (nums.length === 1) return nums;
+    
     let i = 0;
-    for (const num of nums) {
-        if (num !== 0) {
-            nums[i] = num;
+    let j = 0;
+    let len = nums.length;
+    
+    
+    while (i < len) {
+        if (j === nums.length) break;
+        
+        if (nums[i] === 0) {
+            nums.splice(i, 1);
+            nums.push(0);
+            j++;
+        } else {
             i++;
         }
     }
-    nums.fill(0, i)
+    
     return nums;
 };
