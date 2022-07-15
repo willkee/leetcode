21518bc0a896fcd3541c320f2f865db20cfd5783
Will Kee <93359204+willkee@users.noreply.github.com>
2022-07-15 15:37:26 -0600

    Time: 158 ms (80.43%), Space: 52.8 MB (79.29%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -3,12 +3,6 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(words) {
-    const pairs = [];
-    
-    // for (const word of strs) {
-    //     const sortedWord = word.split("").sort().join("");
-    //     pairs.push([word, sortedWord])
-    // }
     const groups = {};
     
     for (const originalWord of words) {
@@ -17,13 +11,6 @@ var groupAnagrams = function(words) {
         if (groups[sortedWord]) groups[sortedWord].push(originalWord);
         else groups[sortedWord] = [originalWord]
     }
-    
-//     for (const pair of pairs) {
-//         const originalWord = pair[0];
-//         const sortedWord = pair[1];
-              
-//         if (groups[sortedWord]) groups[sortedWord].push(originalWord);
-//         else groups[sortedWord] = [originalWord]
-//     }
+
     return Object.values(groups)
 };
