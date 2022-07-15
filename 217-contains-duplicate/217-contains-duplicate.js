6c7446dd04f0ee4290661bcd7e7f07ff0de8172b
Will Kee <93359204+willkee@users.noreply.github.com>
2022-07-15 12:49:54 -0600

    Time: 248 ms (20.35%), Space: 54.8 MB (5.12%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -2,29 +2,34 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-// var containsDuplicate = function(nums) {
-//     /*
-//     Prediction:
-//     Time: O(n)
-//     Space: O(n)
+var containsDuplicate = function(nums) {
+    /*
     
-//     Initialize an empty object
-//     Iterate through the array
-//     If the key does not exist in the obj, add to object
-//     If key exists, return true
-//     return false outside of the for loop
+    Create an object/hash map that will start off as empty.
+    Start a for loop and iterate through the nums array.
+    For each index in the array, we check to see if the object has that value at that index as a key.
+        - If it does, increment the count.
+        - If it does not, create a key with that value and assign a value of 1.
     
-
-//     */
-//     return Array.from(new Set(nums)).length !== nums.length
-
-//     // for (const num of nums) {
-//     //     if (!unique.has(num)) {
-//     //         unique.add(num)
-//     //     } else {
-//     //         return true
-//     //     }
-//     // }
-//     // return false
-// };
- const containsDuplicate = nums => Array.from(new Set(nums)).length !== nums.length
+    For loop to iterate through the object.
+    If any value in the k/v pairs are >= 2, return true.
+    If we make it through the whole object without returning true, return false.
+    
+    Time complexity: O(n) where n is the number of elements in the nums array.
+    Space complexity: O(n) where n is the number of unique values in the nums array.
+    
+    */
+    
+    const hashMap = {};
+    
+    for (const val of nums) {
+        if (hashMap[val]) hashMap[val]++;
+        else hashMap[val] = 1;
+    }
+    
+    for (const key in hashMap) {
+        if (hashMap[key] >= 2) return true;
+    }
+    
+    return false;
+};
