2b2f21b39ce6444638c6144affd6950400d5651b
Will Kee <93359204+willkee@users.noreply.github.com>
2022-07-17 15:48:42 -0600

    Time: 100 ms (63.56%), Space: 47.1 MB (32.23%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -5,18 +5,41 @@
 var isPalindrome = function(s) {
     const alphaNums = new Set('abcdefghijklmnopqrstuvwxyz0123456789'.split(""));
     
-    let newStr = "";
-    for (const char of s) {
-        const lowerChar = char.toLowerCase();
+//     let newStr = "";
+//     for (const char of s) {
+//         const lowerChar = char.toLowerCase();
         
-        if (alphaNums.has(lowerChar)) newStr += lowerChar;
-    }
+//         if (alphaNums.has(lowerChar)) newStr += lowerChar;
+//     }
+    
+//     let left = 0;
+//     let right = newStr.length - 1;
+    
+//     while (left <= right) {
+//         if (newStr[left] !== newStr[right]) return false;
+//         left++;
+//         right--;
+//     }
+    
+//     return true;
     
     let left = 0;
-    let right = newStr.length - 1;
+    let right = s.length - 1;
     
     while (left <= right) {
-        if (newStr[left] !== newStr[right]) return false;
+        const lChar = s[left].toLowerCase();
+        const rChar = s[right].toLowerCase();
+        
+        if (!alphaNums.has(lChar)) {
+            left++;
+            continue;
+        }
+        if (!alphaNums.has(rChar)) {
+            right--;
+            continue;
+        }
+        
+        if (lChar !== rChar) return false;
         left++;
         right--;
     }
