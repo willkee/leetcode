f6180a6ecc60d1f2c0c1921abe082255a721369c
Will Kee <93359204+willkee@users.noreply.github.com>
2022-07-20 08:07:33 -0600

    Time: 131 ms (6.30%), Space: 44.4 MB (43.54%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.js
+++ 206-reverse-linked-list/206-reverse-linked-list.js
@@ -13,7 +13,7 @@ var reverseList = function(head) {
     let prev = null;
     
     while (head) {
-        let next = head.next;
+        const next = head.next;
         head.next = prev;
         prev = head;
         head = next;
