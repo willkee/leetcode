1148a136a64e7d444c516d8a890d8d4a5c96f151
Will Kee <93359204+willkee@users.noreply.github.com>
2022-07-29 10:11:36 -0600

    Time: 100 ms (26.51%), Space: 42.2 MB (46.11%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -7,18 +7,27 @@ var searchInsert = function(nums, target) {
     if (target < nums[0]) return 0;
     if (target > nums[nums.length - 1]) return nums.length;
     
-    const numSet = new Set(nums);
-    numSet.add(target);
+//     const numSet = new Set(nums);
+//     numSet.add(target);
     
-    const newNums = Array.from(numSet).sort((a, b) => a - b);
+//     const newNums = Array.from(numSet).sort((a, b) => a - b);
     
     let l = 0;
     let r = nums.length - 1;
     
     while (l <= r) {
         const mid = Math.floor((l+r)/2);
-        if (newNums[mid] === target) return mid;
-        if (newNums[mid] < target) {
+        if (nums[mid] === target) return mid;
+        
+        if (nums[mid] < target && nums[mid + 1] > target) {
+            return mid + 1;
+        }
+        
+        if (nums[mid] > target && nums[mid - 1] < target) {
+            return mid;
+        }
+        
+        if (nums[mid] < target) {
             l = mid + 1;
         } else {
             r = mid - 1;
