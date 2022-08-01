9e38098544d70a201d682e26368ff84dbeccdbb9
Will Kee <93359204+willkee@users.noreply.github.com>
2022-07-31 20:32:20 -0600

    Time: 132 ms (60.17%), Space: 49.7 MB (32.24%) - LeetHub

--- 344-reverse-string/344-reverse-string.js
+++ 344-reverse-string/344-reverse-string.js
@@ -3,14 +3,5 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    // return s.reverse()
-    let l = 0;
-    let r = s.length - 1;
-    
-    while (l <= r) {
-        [s[l], s[r]] = [s[r], s[l]]
-        l++;
-        r--;
-    }
-    return s;
+    s = s.reverse();
 };
