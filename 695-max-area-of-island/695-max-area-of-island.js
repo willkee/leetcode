6dc164b71452995b914cce7b5266c2c378d16e96
Will Kee <93359204+willkee@users.noreply.github.com>
2022-08-03 20:33:02 -0600

    Time: 108 ms (69.83%), Space: 45 MB (73.81%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.js
+++ 695-max-area-of-island/695-max-area-of-island.js
@@ -4,22 +4,22 @@
  */
 var maxAreaOfIsland = function(grid) {
     let maxArea = 0;
-    let maxRows = grid.length - 1;
-    let maxCols = grid[0].length - 1;
+    let maxR = grid.length - 1;
+    let maxC = grid[0].length - 1;
     
     const dfs = (r, c) => {
         if (r < 0 ||
            c < 0 ||
-           r > maxRows ||
-           c > maxCols ||
+           r > maxR ||
+           c > maxC ||
            grid[r][c] === 0) return 0;
         
         grid[r][c] = 0;
         return 1 + dfs(r-1, c) + dfs(r+1, c) + dfs(r, c-1) + dfs(r, c+1);
     }
     
-    for (let r = 0; r <= maxRows; r++) {
-        for (let c = 0; c <= maxCols; c++) {
+    for (let r = 0; r <= maxR; r++) {
+        for (let c = 0; c <= maxC; c++) {
             if (grid[r][c] !== 0) {
                 maxArea = Math.max(maxArea, dfs(r, c))
             }
