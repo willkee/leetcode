b5d829a5da02257a3aa2ca1e510f895b711c6ba3
Will Kee <93359204+willkee@users.noreply.github.com>
2022-08-03 19:50:45 -0600

    Time: 200 ms (5.31%), Space: 50.2 MB (5.47%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.js
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.js
@@ -13,29 +13,28 @@
 var removeNthFromEnd = function(head, n) {
     if (!head.next) return null;
     
-    let current = head;
-    let length = 0;
-    
-    while (current) {
-        length++;
-        current = current.next;
+    let len = 0;
+    let curr = head;
+    while (curr) {
+        len++;
+        curr = curr.next;
     }
     
-    let target = length - n;
-    let new_current = head;
-    let new_count = 0;
+    const fromStart = len - n;
     
-    if (target === 0) {
-        head = head.next;
-        return head;
-    }
+    let currIdx = 0;
+    let curr2 = head;
     
-    while (new_current) {
-        if (new_count + 1 === target) {
-            new_current.next = new_current.next.next;
-            return head;
+    while (curr2) {
+        console.log(curr2, fromStart)
+        if (fromStart === 0) {
+            return head.next;
         }
-        new_count++;
-        new_current = new_current.next;
+        if (currIdx + 1 === fromStart) {
+            curr2.next = curr2.next.next;
+        }
+        currIdx++;
+        curr2 = curr2.next;
     }
+    return head;
 };
