f34e5d1f9d4ee1af4d0daeba2707ff2f2594cffd
Will Kee <93359204+willkee@users.noreply.github.com>
2022-08-05 10:39:22 -0600

    Time: 170 ms (28.08%), Space: 49.6 MB (96.80%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.js
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.js
@@ -12,7 +12,6 @@
  * @return {TreeNode}
  */
 var mergeTrees = function(root1, root2) {
-    if (!root1 && !root2) return null;
     if (!root1) return root2;
     if (!root2) return root1;
     
@@ -26,26 +25,10 @@ var mergeTrees = function(root1, root2) {
         
         if (!c1.left) c1.left = c2.left
         else stack.push([c1.left, c2.left]);
-        
-        // if (c1.left && c2.left) {
-        //     stack.push([c1.left, c2.left]);
-        // } else if (c1.left && !c2.left) {
-        //     stack.push([c1.left, null]);
-        // } else if (!c1.left && c2.left) {
-        //     c1.left = c2.left
-        //     stack.push([null, c2.left]);
-        // }
+
         
         if (!c1.right) c1.right = c2.right
         else stack.push([c1.right, c2.right]);
-        // if (c1.right && c2.right) {
-        //     stack.push([c1.right, c2.right]);
-        // } else if (c1.right && !c2.right) {
-        //     stack.push([c1.right, null]);
-        // } else if (!c1.right && c2.right) {
-        //     c1.right = c2.right
-        //     stack.push([null, c2.right]);
-        // }
     }
     
     return root1;
