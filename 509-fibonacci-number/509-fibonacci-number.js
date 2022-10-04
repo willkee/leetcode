bcc84a0dcdc11cf32d44d3e522004e6f90e1f982
Will Kee <93359204+willkee@users.noreply.github.com>
2022-10-04 17:22:26 -0600

    Time: 99 ms (55.22%), Space: 41.9 MB (56.56%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -6,7 +6,7 @@ var fib = function(n) {
     const cache = {};
     
     const helper = (n) => {
-        if (n === 0 || n === 1) return n;
+        if (n < 2) return n;
         return cache[n] ? cache[n] : cache[n] = helper(n - 1) + helper(n - 2)
     }
     
