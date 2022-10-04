70d80f328c42c772c630277e7508373b3f86610c
Will Kee <93359204+willkee@users.noreply.github.com>
2022-10-04 17:22:03 -0600

    Time: 60 ms (97.05%), Space: 41.7 MB (68.82%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -3,13 +3,14 @@
  * @return {number}
  */
 var fib = function(n) {
-    if (n === 0 || n === 1) return n;
     const cache = {};
     
-    if (cache[n]) {
-        return cache[n];
-    } else {
-        return cache[n] = fib(n - 1) + fib(n - 2);
+    const helper = (n) => {
+        if (n === 0 || n === 1) return n;
+        return cache[n] ? cache[n] : cache[n] = helper(n - 1) + helper(n - 2)
     }
-    // return fib(n - 1) + fib(n - 2);
+    
+    return helper(n);
+
+
 };
