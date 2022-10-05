7af85eb02d09a181f27fd057158f38d7827c45e8
Will Kee <93359204+willkee@users.noreply.github.com>
2022-10-05 11:55:30 -0600

    Time: 245 ms (41.32%), Space: 42.5 MB (97.64%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.js
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.js
@@ -3,50 +3,22 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-    /*
-    
-    Edge/Corner Cases:
-    if s.length === 1, return s
-    
-    left pointer, right pointer
-    while left < right
-    s[left] !== s[right] 
-    
-    left++
-    right++
-    
-    Time complexity: O(n^2 * log(n))
-    Space complexity: O(1)
-    */
     let max = "";
     
     for (let i = 0; i < s.length; i++) {
-        
         for (let j = 0; j < 2; j++) {
             let left = i;
             let right = i + j;
             
-            while (s[left] && s[left] === s[right]) {
+            while (s[right] && s[left] === s[right]) {
                 left--;
                 right++;
             }
             
-            if (right - left - 1 > max.length) {
+            if (right - left - 1 >= max.length) {
                 max = s.slice(left + 1, right);
             }
         }
-//         let start = s[i];
-//         for (let j = i + 1; j < s.length; j++) {
-//             let end = s[j];
-//             const subString = s.slice(i, j + 1);
-            
-//             if (isPalindrome(subString) && 
-//                 subString.length > longestSubString.length) {
-//                 longestSubString = subString;
-//             }
-//         }
-//     }
-//     return longestSubString;
     };
     return max
 }
