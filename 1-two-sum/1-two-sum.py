93b844ba0e04613c2b95977642930f66ee0cc172
Will Kee <93359204+willkee@users.noreply.github.com>
2022-10-05 15:43:24 -0600

    Time: 64 ms (94.66%), Space: 15.5 MB (8.99%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,45 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # start with 2 pointers
-        # from the 2 pointers in the list, get the middle
-        
-        # for i in range(len(nums)):
-        #     for j in range(len(nums)):
-        #         if (i != j):
-        #             if (nums[i] + nums[j] == target):
-        #                 return [i, j]
-        
-        # sortedNums = nums.copy();
         uniqueNums = set(nums)
-        
         for i in range(len(nums)):
             remainder = target - nums[i]
-            if remainder in uniqueNums and nums.index(remainder) != i:
+            if remainder in uniqueNums and nums.index(remainder) is not i:
                 return [i, nums.index(remainder)]
-            
-            
-        # for (let i = 0; i < nums.length; i++) {
-        # let remainder = target - nums[i];
-        # if (numsSet.has(remainder) && nums.indexOf(remainder) !== i) {
-        #     return [i, nums.indexOf(remainder)]
-        # }
-        # }
-    
-#         sortedNums.sort();
-        
-#         left = 0
-#         right = len(sortedNums) - 1
-#         while (left < right):
-#             print(left, right)
-#             if (sortedNums[left] + sortedNums[right] == target):
-#                 leftIndex = nums.index(sortedNums[left])
-#                 rightIndex = nums.index(sortedNums[right])
-#                 return [leftIndex, rightIndex]
-            
-#             if (sortedNums[left] + sortedNums[right] < target):
-#                 left += 1
-#             elif (sortedNums[left] + sortedNums[right] > target):
-#                 right -= 1
+
         
             
         
